71e0ba481b0e089c90a948f18a17d90420474b22
Sushant Gurav <sushantguravfsd@gmail.com>
2025-09-26 19:54:43 +0530

    Add files via upload

---
+++ basic example3.py
@@ -0,0 +1,5 @@
+#print ("Sushant"+1)
+
+
+print ("Sushant" + " Gurav")
+
--- example1.py
+++ example1.py
@@ -1,5 +1,5 @@
-# Write a Python function to find the maximum element in a list 
-
-x = [30,32,44,60]
-print(max(x))
+print("Sushant")
+print("ajara")
+print("416-505")
+print("7770007338") 
 
